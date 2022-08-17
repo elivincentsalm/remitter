c1adddda26dfca7111205c038ce0c45503e23b78
Eli Salm <elivincentsalm@gmail.com>
2022-08-17 00:14:55 -0500

    ssh from home only

--- modules/http-redir/security_groups.tf
+++ modules/http-redir/security_groups.tf
@@ -7,8 +7,8 @@ resource "aws_security_group" "http-redir-sg" {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      // cidr_blocks = ["${data.external.get_public_ip.result["ip"]}/32"]
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["${data.external.get_public_ip.result["ip"]}/32"]
+      // cidr_blocks = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
       prefix_list_ids = []
       security_groups = []
